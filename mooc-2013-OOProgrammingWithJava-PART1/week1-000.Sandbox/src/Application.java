5cec00712b03ed63ca477fc14cf6736f5739bd57
Jim Tuttle <jim@braggtown.com>
2019-07-30 18:55:02 -0400

    complete Week1-001, week1-003

--- mooc-2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-000.Sandbox/src/Application.java
@@ -5,8 +5,14 @@ public class Application {
 
     public static void main(String[] args) {
 
-        // Write the code here. You can run the code by 
-        // selecting Run->Run File from the menu or by pressing Shift+F6
+
+        System.out.print("    *   \n");
+        System.out.print("   ***  \n");
+        System.out.print("  ***** \n");
+        System.out.print(" ******* \n");
+        System.out.print("*********\n");
+        System.out.print("    *   \n");
+
 
     }
 }
--- mooc-2013-OOProgrammingWithJava-PART1/week1-001.Name/src/Name.java
+++ mooc-2013-OOProgrammingWithJava-PART1/week1-001.Name/src/Name.java
@@ -1,7 +1,7 @@
 public class Name {
     
     public static void main(String[] args) {
-        // Write your program here
+        System.out.println("Jimu Carupinera");
         // Please answer to our survey http://oo-start.mooc.fi/english_mooc_participants/new
         // It will take less than 5 minutes!
 
